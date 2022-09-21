4d46ea6b687a25b97623e3ea4b19349c3af1146a
kimwoojin <waterather21@gaeasoft.co.kr>
2022-09-21 12:19:08 +0900

    Add Dataset code

--- example_1/open_data.py
+++ example_1/open_data.py
@@ -22,5 +22,6 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 def load_housing_data(housing_path=HOUSING_PATH):
      csv_path = os.path.join(housing_path,"housing.csv")
      return pd.read_csv(csv_path)
-
+     
+fetch_housing_data()
 housing = load_housing_data()
